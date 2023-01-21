63dd251de9eea44dea6bb87b5f16ef2107ad6338
alexvmcleod <alexvmcleod@gmail.com>
2023-01-20 20:04:31 -0500

    added hall test script

--- test scripts/hall_test.py
+++ test scripts/hall_test.py
@@ -1,2 +1,16 @@
 import RPi.GPIO as GPIO  # sudo apt-get install python-rpi.gpio
 import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+hallthing = 17
+GPIO.setup(hallthing, GPIO.IN)
+
+while True:
+    ison = GPIO.input(hallthing)
+    if ison == 0:
+        print("oof")
+    else:
+        print('nothing')
+    time.sleep(0.01)
