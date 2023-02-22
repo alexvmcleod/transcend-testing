ecc6406c037261b19b646dd06a6cc999bf065421
alex <alexvmcleod@gmail.com>
2023-02-22 16:34:52 -0500

    added current conversion function with ratios

--- currentsense.py
+++ currentsense.py
@@ -13,6 +13,9 @@ class CurrentSensor:
         self.counterinterval = 100
         self.voltagevallist = []
 
+        self.VRATIO = 1.0/0.305 #ratio of V_MULTIMETER/V_CHIP (raw_val() in this case)
+        self.CRATIO = 8.5 #using a 1000 ohm resistor, the BTS7960's output current should be self.CRATIO * self.VRATIO * raw_val()
+
         self.ldr = MCP3008(channel=currentsensechannel, clock_pin=self.CLK, mosi_pin=self.MOSI, miso_pin=self.MISO, select_pin=self.CS)
 
     def raw_val(self):
@@ -22,11 +25,14 @@ class CurrentSensor:
         self.voltagevallist.append(self.raw_val())
         self.counterval += 1
         
-        if self.counterval % 100 == 0:
+        if self.counterval % self.counterinterval == 0:
             intervalaverage = sum(self.voltagevallist[-self.counterinterval:])/self.counterinterval
             print(f"DA MOVING AVERAGE IS:  {intervalaverage}")
-            
 
+    def convertVtoA(self, v):
+        return v * self.VRATIO * self.CRATIO
 
-    
-
+if __name__ == "__main__":
+    thing = CurrentSensor()
+    print(thing.VRATIO)
+    print(thing.convertVtoA(0.415))
